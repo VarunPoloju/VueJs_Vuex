eb0e675d566fb60ace35633bdf4d542af7647908
varun <varunpoloju966@gmail.com>
2024-05-14 12:31:56 +0530

    mutations -way of changing data

---
+++ src/components/ChangeCounter.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="addOne">Add 1 change counter</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    addOne() {
+      this.$store.commit('increment');
+    },
+  },
+};
+</script>
